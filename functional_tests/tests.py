c0e4373c6f6af72c1cbc59593a5f83c6dbf5a71e
钟鑫 <490336534@qq.com>
2020-12-19 14:23:43 +0800

    使用显式等待替代time.sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,10 @@
 import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -17,10 +20,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        t0 = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - t0 > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -39,13 +50,13 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('购买假蝇')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: 购买假蝇')
+        self.wait_for_row_in_list_table('1: 购买假蝇')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('使用孔雀羽毛做假蝇')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: 购买假蝇')
-        self.check_for_row_in_list_table('2: 使用孔雀羽毛做假蝇')
+        self.wait_for_row_in_list_table('1: 购买假蝇')
+        self.wait_for_row_in_list_table('2: 使用孔雀羽毛做假蝇')
 
         self.fail('Finish the test!')
