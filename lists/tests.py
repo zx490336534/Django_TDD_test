01547348bd26551e72d047a385fb7a4acb0c459a
钟鑫 <490336534@qq.com>
2020-12-19 10:32:57 +0800

    直接通过测试assertTemplateUsed代替之前的self.assertEqual(found.func, home_page)

--- lists/tests.py
+++ lists/tests.py
@@ -6,9 +6,9 @@ from lists.views import home_page
 
 class SmokeTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
