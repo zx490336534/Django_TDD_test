6c696e691650b425275826c4522ab16f8f971102
钟鑫 <490336534@qq.com>
2020-12-19 09:54:25 +0800

    将项目名称修改为superlists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,4 +1,4 @@
-"""Python测试驱动开发 URL Configuration
+"""superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Python测试驱动开发 project.
+WSGI config for superlists project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Python测试驱动开发.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superlists.settings')
 
 application = get_wsgi_application()
