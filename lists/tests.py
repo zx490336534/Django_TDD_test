486b1a6a8cef5130aeb49832c3783df8f519a01c
钟鑫 <490336534@qq.com>
2020-12-19 11:46:07 +0800

    将数据存入数据库 从数据库中读取数据并展示 增加展示前的序号

--- lists/tests.py
+++ lists/tests.py
@@ -11,6 +11,7 @@ class SmokeTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': "一项新事项"})
         self.assertEqual(Item.objects.count(), 1)
+        print(Item.objects.all())
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "一项新事项")
         self.assertIn("一项新事项", response.content.decode())
@@ -40,3 +41,22 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessart(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': '一个新待办事项'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '一个新待办事项')
+
+    def test_refirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': '一个新待办事项1'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='事项1')
+        Item.objects.create(text='事项2')
+        response = self.client.get('/')
+
+        self.assertIn('事项1', response.content.decode())
+        self.assertIn('事项2', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from lists.models import Item
 
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
-
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', new_item_text)
-    })
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    print(items)
+    return render(request, 'home.html', {'items': items})
