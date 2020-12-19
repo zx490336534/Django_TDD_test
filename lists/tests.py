6a3888e29a7b409c5ce1f1f5f8557e218f76007b
钟鑫 <490336534@qq.com>
2020-12-19 11:03:48 +0800

    ORM数据库迁移

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from lists.models import Item
 
 
 class SmokeTest(TestCase):
@@ -14,3 +15,22 @@ class SmokeTest(TestCase):
         response = self.client.post('/', data={'item_text': "一项事项"})
         self.assertIn('一项事项', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    def test_saveing_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '第一条待办事项'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '第二条待办事项'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item, '第一条待办事项')
+        self.assertEqual(second_saved_item, '第二条待办事项')
