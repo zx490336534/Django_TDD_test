30e838244ccf4f5bae26931ead397deda465c39d
钟鑫 <490336534@qq.com>
2020-12-20 16:40:37 +0800

    7.9.2 根据新模型定义调整代码

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
-
-class Item(models.Model):
-    text = models.TextField(default='')
+from django.db.models import CASCADE
 
 
 class List(models.Model):
     pass
+
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=CASCADE)
