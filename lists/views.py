f4d6069976f7b7b63f1fd88bda9541e17a1b5448
钟鑫 <490336534@qq.com>
2020-12-20 16:59:43 +0800

    7.12.3 完成add_item编码

--- lists/views.py
+++ lists/views.py
@@ -17,3 +17,9 @@ def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
