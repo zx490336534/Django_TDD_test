6cdad2cd5787caf3bbd060823460af8e4be739fe
钟鑫 <490336534@qq.com>
2020-12-20 16:29:58 +0800

    7.9 调整模型

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
+
+
+class List(models.Model):
+    pass
--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,24 @@
 # 运行单元测试:python manage.py test lists
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saveing_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         first_item = Item()
         first_item.text = '第一条待办事项'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '第二条待办事项'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -20,6 +27,8 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '第一条待办事项')
         self.assertEqual(second_saved_item.text, '第二条待办事项')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class HomePageTest(TestCase):
