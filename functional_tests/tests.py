1755ef3c1d60aac052e3ea222c8b4fa171656c9f
钟鑫 <490336534@qq.com>
2020-12-27 10:30:08 +0800

    8.1 新增css居中测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -93,3 +93,13 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('购买羽毛', page_text)
         self.assertIn('购买牛奶', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('item_text')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
