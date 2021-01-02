19f1a38a9def2121cb68b1eb10a68aa2b0cbf2ee
钟鑫 <490336534@qq.com>
2021-01-02 19:34:37 +0800

    修改host

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -91,11 +91,11 @@ def _update_database(source_folder):
 
 
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
+    site_folder = f'/home/{env.user}/sites/zhongxinTDD'
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
+    _update_settings(source_folder, "zhongxinTDD")
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
