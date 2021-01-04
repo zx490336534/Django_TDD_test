3393100ab870b6a79a8070d7a0695afc681ab017
钟鑫 <490336534@qq.com>
2021-01-04 21:53:24 +0800

    拆分单元测试

---
+++ lists/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/4 下午9:50
+# @Author  : zhongxin
+# @Email   : 490336534@qq.com
+# @File    : __init__.py.py
--- lists/tests/tests_views.py
+++ lists/tests/tests_views.py
@@ -3,34 +3,6 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class ListAndItemModelsTest(TestCase):
-    def test_saveing_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        first_item = Item()
-        first_item.text = '第一条待办事项'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '第二条待办事项'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '第一条待办事项')
-        self.assertEqual(second_saved_item.text, '第二条待办事项')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
