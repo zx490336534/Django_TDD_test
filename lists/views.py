c52abce75ee597011ddfd966f21ecbf7586ae76d
钟鑫 <490336534@qq.com>
2021-01-05 22:16:18 +0800

    14.2.1 html使用表单自动生成HTML input元素

--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.utils.html import escape
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
