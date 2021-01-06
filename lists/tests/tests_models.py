f7dca9de6f61456d78ec4546f02d2d714632a39e
钟鑫 <490336534@qq.com>
2021-01-06 19:28:42 +0800

    15.1.3 重写旧模型测试

--- lists/tests/tests_models.py
+++ lists/tests/tests_models.py
@@ -4,32 +4,17 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class ListAndItemModelsTest(TestCase):
-    def test_saveing_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        first_item = Item()
-        first_item.text = '第一条待办事项'
-        first_item.list = list_
-        first_item.save()
+class ItemModelTest(TestCase):
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        second_item = Item()
-        second_item.text = '第二条待办事项'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '第一条待办事项')
-        self.assertEqual(second_saved_item.text, '第二条待办事项')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.list, list_)
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -38,10 +23,6 @@ class ListAndItemModelsTest(TestCase):
             # 检查做某件事是否会抛出异常
             item.save()
             item.full_clean()
-
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -70,3 +51,9 @@ class ListAndItemModelsTest(TestCase):
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+
+class ListModelTest(TestCase):
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
