70c6de226b2383121efed362de158a4cb55d6e5e
钟鑫 <490336534@qq.com>
2021-01-07 22:43:19 +0800

    16 为base.html添加list.js

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,8 +1,6 @@
-var initialize = function () {
-    console.log('initialize called');
+window.Superlists = {};
+window.Superlists.initialize = function () {
     $('input[name="text"]').on('keypress', function () {
-        console.log('in keypress handler');
         $('.has-error').hide()
     })
 };
-console.log('list.js loaded');
